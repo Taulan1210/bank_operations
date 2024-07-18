9d96a24031b7eefa9d7fa11f3491df5c02bc25f1
Taulan Eraliev <etaulan@mail.ru>
2024-07-18 20:52:09 +0300

    initial

--- src/config.py
+++ src/config.py
@@ -1,1 +1,3 @@
+import os
 
+data_file_name = os.path.join('operations.json')
--- src/function.py
+++ src/function.py
@@ -1,26 +1,54 @@
+from __future__ import annotations
+
 import json
-
-from dto import Operation
-
-
-def get_operations(filename) -> list[Operation]:
-    operations: list[Operation] = []
-    with open(filename, encoding='utf-8') as f:
-        for data in json.load(f):
-            if data:
-                op = Operation.__init__from__dict(data)
-                operations.append(op)
-
-    return operations
+import datetime
 
 
-def filter_operation_by_state(*operations: Operation, state: str) -> list[Operation]:
-    filtered_operations: list[Operation] = []
-    for op in operations:
-        if op.state == state:
-            filtered_operations.append(op)
-    return filtered_operations
+def get_data_json(file_name) -> list:
+    with open(file_name, 'r', encoding="utf-8") as file:
+        return json.load(file)
 
 
-def sort_operation_by_date(*operations: Operation) -> list[Operation]:
-    return sorted(operations, key=lambda op: op.date, reverse=True)
+def sort_status(json_list: list, state: str) -> list:
+    result = []
+    for i in json_list:
+        if i.get('state') == state:
+            result.append(i)
+    return result
+
+
+def sort_dates(list_: list) -> list:
+    return sorted(list_, key=lambda x: x['date'])
+
+
+def revers(list_: list) -> list:
+    return list_[::-1]
+
+
+def format_date(value: str) -> str:
+    date = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+    return date.strftime('%d.%m.%Y')
+
+
+def get_from_to(dict_: dict, from_: str, to_: str) -> (bool, str):
+    from_transaction = dict_.get(from_)
+    to_transaction = dict_.get(to_)
+    if from_transaction is None:
+        return False, to_transaction
+    return from_transaction, to_transaction
+
+
+def cipher(str_: str | bool) -> str | None:
+
+    if not str_:
+        return None
+    elif "счет" in str_.lower():
+        _name = str_.split()[:-1]
+        _number = str_.split()[-1]
+        result = f"{' '.join(_name)} **{_number[-4:]}"
+        return result
+    else:
+        card_name = str_.split()[:-1]
+        card_number = str_.split()[-1]
+        result = f"{' '.join(card_name)} {card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+        return result
